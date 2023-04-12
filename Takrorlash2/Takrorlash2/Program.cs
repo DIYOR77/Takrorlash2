704b7ba61b739ec3cc9211b6dfc567b7c426aa22
unknown <129381997+DIYOR77@users.noreply.github.com>
2023-04-12 15:56:56 +0500

    Satrdan xarflarini sanash

--- Takrorlash2/Takrorlash2/Program.cs
+++ Takrorlash2/Takrorlash2/Program.cs
@@ -1,10 +1,40 @@
-﻿namespace Takrorlash2
+﻿using System;
+
+namespace Takrorlash2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(" Enter string: ");
+            string str=Console.ReadLine();
+            Xarflarnisanash(str);
+            
+        }
+        public static void Xarflarnisanash(string str)
+        {
+            char[]arr = str.ToCharArray();
+            Array.Sort(arr);
+            int count = 1;
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (i == arr.Length-1  )
+                {
+                   Console.WriteLine($"{arr[i - 1]}: {count+1}");
+                }
+                if (arr[i] == arr[i - 1])
+                {
+                    count++;
+                }
+                else
+                {
+                    Console.WriteLine($"{arr[i-1]}: {count}");
+                    count = 1;
+                }
+            }
+            
+
+
         }
     }
 }
