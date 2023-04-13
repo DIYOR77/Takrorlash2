f7623970e36209c158f6729335fffb1d8bc4ae4b
unknown <129381997+DIYOR77@users.noreply.github.com>
2023-04-13 15:47:41 +0500

    Eng ko'p ketma ketgan y ni sonini topish.

--- Takrorlash2/Takrorlash2/Program.cs
+++ Takrorlash2/Takrorlash2/Program.cs
@@ -1,10 +1,38 @@
-﻿namespace Takrorlash2
+﻿using System.Data.SqlTypes;
+
+namespace Takrorlash2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("  Enter string: ");
+            string str=Console.ReadLine();
+            Engkupketmaketkelgan(str);
+        }
+        public static void Engkupketmaketkelgan(string str)
+        {
+            int count = 1;
+            int max = 0;
+            for(int i=0; i<str.Length; i++)
+            {
+                if (str[i] == 'y')
+                {
+                        if (str[i] == str[i + 1])
+                        {
+                            count++;
+                        }
+                        else
+                        {
+                            if (max < count)
+                            {
+                                max = count;
+                            }
+                            count = 1;
+                        }
+                }
+            }
+            Console.WriteLine(" Eng ko'p ishlatilingan y soni  "+max);
         }
     }
 }
