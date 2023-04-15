dbfe0883b6d4f4347e9873a95f31ec2b5632b0de
unknown <129381997+DIYOR77@users.noreply.github.com>
2023-04-15 12:54:19 +0500

    Massivedagi juft indexdagi toq va toq indexdagi juft elementni topish.

--- Takrorlash2/Takrorlash2/Program.cs
+++ Takrorlash2/Takrorlash2/Program.cs
@@ -1,10 +1,64 @@
-﻿namespace Takrorlash2
+﻿using System.Data.SqlTypes;
+
+namespace Takrorlash2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write(" Enter massive size: ");
+            int n=int .Parse(Console.ReadLine());
+            int[]arr = new int[n];
+            Fillmasiveand(arr);
+
+        }
+        public static void Fillmasiveand(int[]arr)
+        {
+            Random random= new Random();    
+            for(int i = 0; i < arr.Length; i++)
+            {
+                arr[i]=random.Next(10,99);
+                Console.WriteLine(" "+i+" -> "+arr[i]);
+            }
+            Console.WriteLine();
+            for (int i=0;i < arr.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    if (arr[i] % 2 == 1)
+                    {
+                        Console.WriteLine(" Juft indexda joylangan toq  element: " + arr[i] +" index: "+i);
+                    }
+                }
+                else  if(i % 2 == 1)
+                {
+                    if (arr[i] %2 == 0)
+                    {
+                        Console.WriteLine(" Toq indexda joylangan juft  element: " + arr[i] + " index: " + i);
+                    }
+                } 
+            }
+            Console.WriteLine();
+            for (int i=0;i<arr.Length;i++)
+            {
+                if (i % 2 == 0 && arr[i]%2==1)
+                {
+                        Console.Write($"{" "}{arr[i] = 0}");
+                }
+                else if (i % 2 == 1 && arr[i]%2==0)
+                {
+                        Console.Write($"{" "} {arr[i] = -1}");
+                }
+                else
+                {
+                    Console.Write(" "+arr[i]);
+                }
+
+
+
+
+            }
         }
     }
+
 }
